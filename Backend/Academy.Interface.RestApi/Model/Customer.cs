c914fe080ac688d1c49c0835e0dbe6b32aef63f6
Teacher <Teacher@Tahlildadeh>
2018-09-14 12:42:24 +0430

    backend initial :muslce:

---
+++ Backend/Academy.Interface.RestApi/Model/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Academy.Interface.RestApi.Model
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
